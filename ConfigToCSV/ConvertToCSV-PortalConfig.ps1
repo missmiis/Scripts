c41afa336988ba6f488034aa20d38c480ce7cca7
missmiis <carol@wapshere.com>
2019-11-15 14:24:13 +1100

    Add files via upload

--- ConfigToCSV/ConvertToCSV-PortalConfig.ps1
+++ ConfigToCSV/ConvertToCSV-PortalConfig.ps1
@@ -5,8 +5,14 @@
 
     Written by Carol Wapshere, http://www.wapshere.com.missmiis
 
-    Takes the policy.xml and schema.xml files exported using the standard MIM Service migration scripts, see https://www.microsoft.com/en-us/download/details.aspx?id=1663
-    Produces semi-colon separated CSV files for each object type. Only attributes suitable for inclusion in the CSV are included (ie excluded embedded XML and values with semicolons in).
+    Takes the policy.xml and schema.xml files exported and converts them to a CSV file per object type.
+    
+    NOTES:
+    - Export policy.xml and schema.xml using the MS process at https://www.microsoft.com/en-us/download/details.aspx?id=1663
+    - Produces semi-colon separated CSV files for each object type. 
+    - Only attributes suitable for inclusion in the CSV are included - ie, excludes embedded XML and values with semicolons in them.
+    - Multi-valued attributes are comma-seperated within the semi-colon delimued format.
+    - Where an attribuite contains a GUID that is listed in the same XML file, the script will swap it for the target object's Display Name.
 #>
 
 $ErrorActionPreference = "Stop"
@@ -34,6 +40,9 @@ foreach ($ConfigFile in $ConfigFiles)
     $csv = Import-csv $TempFile -Delimiter ";"
     $csv = $csv | where {$_.id  -like "urn:uuid:*"}
 
+    $DisplayNames = @{}
+    foreach ($item in ($csv | where {$_.attribute -eq "DisplayName"} | select -Property id,value)) {$DisplayNames.Add($item.id,$item.value)}
+
     $ObjectTypes = $csv.type | select -unique
     foreach ($ObjectType in $ObjectTypes)
     {
@@ -48,7 +57,21 @@ foreach ($ConfigFile in $ConfigFiles)
             $pso = New-Object PSObject
             foreach ($attribute in $attributes)
             {
-                $pso | Add-Member -MemberType NoteProperty -Name $attribute -Value ($objrows | where {$_.attribute -eq $attribute}).value
+                $value = $null
+                try{$value = ($objrows | where {$_.attribute -eq $attribute}).value} catch {}
+                if ($attribute -ne "ObjectID" -and $value -and $value.StartsWith("urn:uuid:"))
+                {
+                    $tmp = $value
+                    foreach ($guid in $tmp.split(","))
+                    {
+                        if ($DisplayNames.ContainsKey($guid))
+                        {
+                            $value = $value.Replace($guid,($DisplayNames.($guid)))
+                        }
+                    }
+                }
+
+                $pso | Add-Member -MemberType NoteProperty -Name $attribute -Value $value
             }
             $arr += $pso
         }
